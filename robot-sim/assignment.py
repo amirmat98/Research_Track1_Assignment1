e0cae1bc3f325ff7bf14408a4b993692180778e8
amirmat98 <matin.amirmahdi@gmail.com>
2023-11-08 23:49:09 +0000

    Define Release Golden Token Function

--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -90,7 +90,7 @@ def gold_grab():
 		print("Found a Gold one!")	 
 		token_flag = 0
 		
-	    elif -angle_threshold<= rotation_y <= a_th: # if the robot is well aligned with the token but not close, we go forward to reach it
+	    elif -angle_threshold<= rotation_y <= angle_threshold: # if the robot is well aligned with the token but not close, we go forward to reach it
 		print("Going forward!.")
 		drive(10, 0.5)
 		
@@ -101,7 +101,29 @@ def gold_grab():
 	    elif rotation_y > angle_threshold:
 		print("Right a bit...")
 		turn(+2, 0.5)
+	
+#This function to move towards the closest drop location ( The closest token which was previously moved and relocated)	
+def release_golden_token():
+
+	flag = 1			
+	while flag:
+	    distance , rotation_y , token_code = find_token_location()  # we look for closest gold token which was droped previously
+	    
+	    if distance < distance_threshold + 0.2:  # if the robot is close enough to the drop location the while loop is stopped so the robot can release the box
+	    
+	    # The value 0.2 is defined so that the robot releases the box it holds a small distance away from the target box
+		print("Found a drop location!")	 
 		
+		flag = 0
+	    elif -angle_threshold <= rotation_y <= angle_threshold: # if the robot is well aligned with the drop location, we go forward
+		print("Going forward!.")
+		drive(10, 0.5)
+	    elif rotation_y < -angle_threshold: # if the robot is not well aligned with the drop location, we move it on the left or on the right
+		print("Left a bit...")
+		turn(-2, 0.5)
+	    elif rotation_y > angle_threshold:
+		print("Right a bit...")
+		turn(+2, 0.5)	
 		
 def main():
 
