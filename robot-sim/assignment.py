adef6ab47f712f831c86c95b25fdef62c5186b5b
amirmat98 <matin.amirmahdi@gmail.com>
2023-11-08 15:21:20 +0000

    Import Libraries

--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -1,9 +1,23 @@
-from __future__ import print_function
+"""
+Descriptions
+"""
 
-import time
-from sr.robot import *
+#import libraries
+from __future__ import print_function # for print()
+import time # for time.sleep()
+from sr.robot import * # for robot object
 
-R = Robot()
+
+# define global variables
+my_robot = Robot() # robot object
+my_time = 0.5 # turn and drive time
+my_speed = 25 # turn and drive speed
+angle_threshold = 2.0 # angle threshold
+distance_threshold = 0.4 # distance threshold
+#silver_token_list = [] # array to store the code of collected silver tokens
+gold_token_list = [] # array to store the code of gold tokeens
+
+
 
 
 while 1:
