600504d326e0c0b7bc205d35abf3c37e704a404f
amirmat98 <matin.amirmahdi@gmail.com>
2023-11-08 16:03:06 +0000

    ِDefine ُSearch Gold Token Function

--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -35,6 +35,29 @@ def turn(speed, seconds):
 	my_robot.motors[0].m0.power = 0
 	my_robot.motors[0].m1.power = 0
 
+#This Function to find the closest Golden Box with no elements in the list "gold_token_list"
+# return: distance, angle and code of the nearest token
+def search_gold_token():
+	distance = 100
+	rotation_y = 0
+	token_code = -1
+	
+	for token in my_robot.see():
+		# if the token is unmarked and in the range of the robot, update the distance, angle and code of token
+		if token.dist<distance and token.info.marker_type == MARKER_TOKEN_GOLD and token.info.code not in gold_token_list:   
+			distance = token.dist
+			token_code = token.info.code
+			rotation_y = token.rot_y
+		
+			
+	if distance >= 100:
+	
+		return -1 , -1 ,token_code
+	
+	else:
+		return distance, rotation_y ,token_code
+		
+
 
 while 1:
     pass
