9758a7c57ddb100111e326011d85256f40085b11
amirmat98 <matin.amirmahdi@gmail.com>
2023-11-08 15:32:34 +0000

    ِDefine Turn Function

--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -25,6 +25,15 @@ def drive(speed, seconds):
 	time.sleep(seconds)
 	my_robot.motors[0].m0.power = 0
 	my_robot.motors[0].m1.power = 0
+	
+	
+# turn the robot left/right
+def turn(speed, seconds):
+	my_robot.motors[0].m0.power = speed
+	my_robot.motors[0].m1.power = -speed
+	time.sleep(seconds)
+	my_robot.motors[0].m0.power = 0
+	my_robot.motors[0].m1.power = 0
 
 
 while 1:
